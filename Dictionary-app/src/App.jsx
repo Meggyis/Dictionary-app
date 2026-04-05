f20ce48170f7de3a2380206c6e94c8612164c690
Meggyis <katalinsimon1413@gmail.com>
2026-04-05 10:35:36 +0200

    Added search engine

--- Dictionary-app/src/App.jsx
+++ Dictionary-app/src/App.jsx
@@ -1,16 +1,23 @@
 import logo from './assets/logo.png'; 
 import './App.css'
+import Dictionary from './Dictionary';
 
 function App() {
 
   return (
    
     <div className="App">
+      <div className='container'>
     <header className='App-header'>
       <img src={logo} className="App-logo img-fluid" alt="Logo" />
    <a href="#" className='btn btn-primary shadow'>Hi</a>
     </header>
-   </div>
+    <main>
+      <Dictionary />
+    </main>
+    <footer className='App-footer'><small>Coded by Katalin Simon for the SheCodes Bootcamp</small></footer>
+    </div>
+  </div>
   )
 }
 
