ac22f7bcce1f2dc13c80baa22c8af228415aad55
Meggyis <katalinsimon1413@gmail.com>
2026-04-05 11:09:21 +0200

    Added SheCodes dictionary API engine

--- Dictionary-app/src/Dictionary.jsx
+++ Dictionary-app/src/Dictionary.jsx
@@ -1,14 +1,20 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
     const [word, setWord] = useState("");
 
     function search(event) {
         event.preventDefault();
-        alert("Searching...");
+        let apiKey = "ta7cf76b03d3d0cfof27fb0472606ea4";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleWordChange(event) {
         setWord(event.target.value);
+    }
+    function handleResponse(response) {
+        console.log(response.data);
     }
   return (
     <div className="Dictionary">
